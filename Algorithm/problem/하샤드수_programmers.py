afc8a153495fca80b86951a0acbb9a112869778d
SangRakKun <leesangrak0307@gmail.com>
2021-01-10 00:52:02 +0900

    20200110 문제추가

---
+++ Algorithm/problem/하샤드수_programmers.py
@@ -0,0 +1,12 @@
+def solution(x):
+    sum=0
+
+    x=str(x)
+    for i in range(len(x)):
+        sum+=int(x[i])
+    x=int(x)
+    
+    if x%sum==0:
+        return True
+    else:
+        return False
